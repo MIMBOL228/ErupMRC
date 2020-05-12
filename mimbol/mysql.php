d39e3bfe59bf7f56851d726d7ba8359dabb4c6c5
mimbol228 <mim.bol@mail.ru>
2020-05-12 13:00:13 +0500

    Маленькая доработка

--- mimbol/mysql.php
+++ mimbol/mysql.php
@@ -1,6 +1,5 @@
 <?php
- class sqlm {
-  public function con() {
+    function con() {
      $dsn = sprintf('mysql:dbname=%s;host=%s;charset=utf8;', $GLOBALS['db']['dbname'], $GLOBALS['db']['ip']);
      $user = $GLOBALS['db']['user'];
      $password = $GLOBALS['db']['password'];
@@ -11,11 +10,11 @@
      }
      return $connection;
     }
-    public function startsWith ($string, $startString){
+    function startsWith ($string, $startString){
      $len = strlen($startString);
      return (substr($string, 0, $len) === $startString);
     }
-    public function query(string $sql, array $parameters){
+    function query(string $sql, array $parameters){
      $statement = con()->prepare($sql);
      $newParameters = [];
      foreach ($parameters as $key => $value) {
@@ -30,10 +29,9 @@
         return $statement->fetch(PDO::FETCH_ASSOC);
      return [];
     }
-    public function table($table,$or=0,$do=99999){
+    function table($table,$or=0,$do=99999){
      $con = mysqli_connect($GLOBALS['db']['ip'],$GLOBALS['db']['user'],$GLOBALS['db']['password'],$GLOBALS['db']['dbname']);
      $result = mysqli_query($con,"SELECT * FROM `$table` LIMIT $or,$do")  or die(mysqli_error($con));
      mysqli_close($con);
      return $result;
     }
-}
